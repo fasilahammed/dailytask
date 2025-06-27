380531b0ed9b2c3bb6a9ce99795895ef07628696
fasilahammed <ahamedahamed1883@gmail.com>
2025-06-27 10:31:48 +0530

    dailytask

---
+++ 27.js
@@ -0,0 +1,19 @@
+function square(x) {
+    return x**2
+}
+console.log(square(5));
+
+
+/////////////////////////////////////
+
+
+ square ((x)=>x**2)
+
+console.log(square(5));
+
+/////////////////////////////////////
+
+let cube= function(x) {
+    return x**3
+}
+console.log(cube(4));
