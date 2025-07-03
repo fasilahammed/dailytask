b3454c049af53e97dec1003f9186c2893d5fc694
fasilahammed <ahamedahamed1883@gmail.com>
2025-07-03 15:14:04 +0530

    dailytask

--- 3.js
+++ 3.js
@@ -1,9 +1,39 @@
-function syncs() {
-    console.log("start");
-    setTimeout(() => {
-        console.log("Async Task");
-     
-    }, 2000);  
-        console.log("end");
+// function syncs() {
+//     console.log("start");
+//     setTimeout(() => {
+//         console.log("Async Task");
+
+//     }, 2000);  
+//         console.log("end");
+// }
+// syncs()
+
+
+////////////////////////////////////////////
+
+function first() {
+
+    /////first global exicution will work
+    console.log("first");
+    ////then syncrunoslyy "first will work"
+
+    function second() {
+       
+        ////then second exicute and syncrounuslyy its log will exicute
+
+        console.log("second");
+
+        function third() {
+
+            ////then third exicute and syncrounusllyy its log will works
+            console.log("third");
+
+        }
+        third()
+
+    }
+    second()
+
 }
-syncs()
+first()
+
