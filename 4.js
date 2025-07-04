0c0aeb9f79c85fee2858a74204cdb24670e56701
fasilahammed <ahamedahamed1883@gmail.com>
2025-07-04 14:33:28 +0530

    dailytask

--- 4.js
+++ 4.js
@@ -1,16 +1,19 @@
 let a=35;
-
+let b=25;
 function shdw() {
     
    
     let a=100 /// legal shadowing 
-
-    console.log(a);
+    var b=500;///ilegal shadowing
+    console.log(a);////legal
+    console.log(b);///ilegal
     
     
 }
 shdw()
 console.log(a);
+console.log(b);
+
 
 ///////////////////////////////
 
